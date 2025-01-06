edc0e29f4a62053895a89723d6f3ddb30f39345d
YangJinzhe <2772972739@qq.com>
2025-01-06 11:51:01 +0800

    创建游戏模式

--- Source/Aura/Private/Character/PlayerCharacter.cpp
+++ Source/Aura/Private/Character/PlayerCharacter.cpp
@@ -2,4 +2,25 @@
 
 
 #include "Character/PlayerCharacter.h"
+#include"GameFramework/CharacterMovementComponent.h"
 
+APlayerCharacter::APlayerCharacter()
+{
+	//角色朝面向方向旋转
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	//旋转的速率
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+
+	//限制角色只能在一个平面内移动
+	GetCharacterMovement()->bConstrainToPlane = true;
+
+	//角色位置和朝向固定在一个平面
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	//角色不跟随控制器旋转
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+
+}
--- Source/Aura/Public/Game/AuraGameModeBase.h
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -3,18 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Character/CharacterBase.h"
-#include "PlayerCharacter.generated.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API APlayerCharacter : public ACharacterBase
+class AURA_API AAuraGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-
-public:
-	APlayerCharacter();
 	
 };
