5a1976ad991d362cc814e1060742013022342772
YangJinzhe <2772972739@qq.com>
2025-01-20 22:21:37 +0800

    添加依赖模块

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -7,7 +7,7 @@ public class Aura : ModuleRules
 	public Aura(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+		//Ìí¼ÓÒÀÀµÄ£¿é
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","EnhancedInput", "GameplayAbilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags","GameplayTasks" });
